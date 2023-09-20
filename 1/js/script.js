15598d2bbe6a759bcac1755f14d953e276c08775
Alexandrminskiy <alexandrminskiy@gmail.com>
2023-09-20 11:32:41 +0300

    first commit

---
+++ 1/js/script.js
@@ -0,0 +1,35 @@
+// const result = confirm('Are you here')
+// console.log(result);
+
+// const answer = prompt('hello', 'hello')
+// console.log(answer);
+
+// const answers = [];
+// answers[0] = prompt('Как Ваше имя', '');
+// answers[1] = prompt('Какая Ваша фамилия', '');
+// answers[2] = prompt('Сколько Вам лет', '');
+// // console.log(answers);
+
+// document.write(answers);
+
+
+// let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// }
+
+// const a = prompt('Один из последних просмотренных фильмов')
+// const b = prompt('На сколько оцените его')
+// const c = prompt('Один из последних просмотренных фильмов')
+// const d = prompt('На сколько оцените его')
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+// console.log(personalMovieDB);
+
+if 
