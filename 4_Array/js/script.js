b7a4ef13f6ded440e0a50ed0d48d5388267fc689
Alexandrminskiy <alexandrminskiy@gmail.com>
2023-10-23 09:49:32 +0300

    123

---
+++ 4_Array/js/script.js
@@ -0,0 +1,34 @@
+const arr = [10, 62, 3, 1, 6, 8];
+
+
+arr.sort((a,b)=>{
+return a - b
+});
+console.log(arr);
+
+
+
+// arr.pop();
+// Удаляет последний элемент
+
+// arr.push(10)
+// Добовляет элемент в конец
+
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// for (let val of arr) {
+//     console.log(val)
+// }
+
+// arr.forEach((item, i, arr)=>{
+// console.log(`${i}: ${item} внутри массива ${arr}`);
+// })
+
+// const str = prompt('', '');
+// const products = str.split(', ');
+// products.sort();
+// console.log(products.join('; '));
