57144fc9e8cc7475a4f073886848d2b42221cad4
Alexandrminskiy <alexandrminskiy@gmail.com>
2024-01-17 16:43:58 +0300

    08

---
+++ 16_date/script.js
@@ -0,0 +1,11 @@
+const now = new Date(now.getTime);
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getHours());
+// console.log(now.getMinutes());
+// console.log(now.getDay());
+
+console.log(now);
+// const date = now.getTime();
+// console.log(date);
